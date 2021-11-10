ec1dd61e72d145c201d9fa2d8d32812a0570bb8a
valenciar527 <45440846+valenciar527@users.noreply.github.com>
2021-11-10 00:06:37 -0500

    draft 2

---
+++ js/audio.js
@@ -0,0 +1,7 @@
+$("#modal_1").on('hidden.bs.modal', function (e) {
+	$("#pause-target-1").attr("src", $("#pause-target-1").attr("src"));
+});
+
+$("#modal_2").on('hidden.bs.modal', function (e) {
+	$("#pause-target-2").attr("src", $("#pause-target-2").attr("src"));
+});
---
+++ js/scroll.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+	$('#scroll-target1').on('click', function() {
+		$('#overlay').animate({
+			opacity: 0,
+		}, 1, function() {
+			// Animation complete.
+		});
+		$('#overlay').animate({
+			opacity: 1,
+		}, 400, function() {
+			// Animation complete.
+		});
+	}); 
+
+	$('#scroll-target2').on('click', function() {
+		$('#overlay').animate({
+			opacity: 0,
+		}, 1, function() {
+			// Animation complete.
+		});
+		$('#overlay').animate({
+			opacity: 1,
+		}, 400, function() {
+			// Animation complete.
+		});
+	}); 
+
+	$('#scroll-target3').on('click', function() {
+		$('#overlay').animate({
+			opacity: 0,
+		}, 1, function() {
+			// Animation complete.
+		});
+		$('#overlay').animate({
+			opacity: 1,
+		}, 400, function() {
+			// Animation complete.
+		});
+	}); 
+
+	$('#scroll-target4').on('click', function() {
+		$('#overlay').animate({
+			opacity: 0,
+		}, 1, function() {
+			// Animation complete.
+		});
+		$('#overlay').animate({
+			opacity: 1,
+		}, 400, function() {
+			// Animation complete.
+		});
+	}); 
+
+	$('#scroll-target5').on('click', function() {
+		$('#overlay').animate({
+			opacity: 0,
+		}, 1, function() {
+			// Animation complete.
+		});
+		$('#overlay').animate({
+			opacity: 1,
+		}, 400, function() {
+			// Animation complete.
+		});
+	}); 
+});
