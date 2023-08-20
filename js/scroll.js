74e4c68565db7496231c3b39b2aefd700d173db3
valenciar527 <45440846+valenciar527@users.noreply.github.com>
2023-08-20 14:51:40 -0400

    new hero section, udpated scroll, added hero section animations, added logo

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-	$('.scroll-target').on('click', function() {
+	$('.scroll-target').on('click', function(e) {
+		e.preventDefault();
 		$('#overlay').animate({
 			opacity: 0,
-		}, 1, function() {
+		}, 400, function() {
 			// Animation complete.
 		});
+		// Adding a delay before navigation
+		setTimeout(function() {
+			window.location.href = $(e.target).attr('href');
+		}, 400); // Delay in milliseconds
+		
 		$('#overlay').animate({
 			opacity: 1,
 		}, 400, function() {
 			// Animation complete.
 		});
 	}); 
-
 });
